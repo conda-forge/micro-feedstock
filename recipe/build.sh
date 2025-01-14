47669629da6c3c467dd92e9d37b71350a009dc5b
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-14 00:19:16 -0800

    update URL and build

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,9 +2,7 @@
 
 set -eoux pipefail
 
-
-make build
-make install
+go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w" ./cmd/micro
 make test
 
 go-licenses save "${SRC_DIR}/cmd/micro" --save_path="${RECIPE_DIR}/thirdparty-licenses/"
