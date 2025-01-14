0738736f0435de7da9ca1487508ec39ce1c13767
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-14 01:48:13 -0800

    Enable platforms (#3)
    * raise MACOSX_SDK_VERSION
    * MNT: Re-rendered with conda-build 24.11.2, conda-smithy 3.45.2, and conda-forge-pinning 2025.01.11.16.15.44
    * fix Windows and cross-compile

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,10 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 
-set -eoux pipefail
+set -o xtrace -o nounset -o pipefail -o errexit
 
-
-make build
-make install
+go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w" ./cmd/micro
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR:-}" != "" ]]; then
 make test
+fi
 
 go-licenses save "${SRC_DIR}/cmd/micro" --save_path="${RECIPE_DIR}/thirdparty-licenses/"
